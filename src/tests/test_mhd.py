3bee3ae56bfb8b16e9b43fb029d5b044eb70b82b
TuuPu <puontit@gmail.com>
2022-04-01 12:01:56 +0300

    Fixed some tests

--- src/tests/test_mhd.py
+++ src/tests/test_mhd.py
@@ -14,10 +14,28 @@ class TestMhd(unittest.TestCase):
         self.edge_testing = image_processing.create_binary_edge_image(self.test_images)
 
     def test_min_distance_pairwise(self):
+        coordinates_test_img = image_processing.coordinates(self.edge_testing[0])
+        coordinates_training_img = image_processing.coordinates(self.edge_training[0])
+        tree1 = cKDTree(coordinates_test_img)
+        tree2 = cKDTree(coordinates_training_img)
+        distance_1 = tree1.query(coordinates_training_img)[0]
+        distance_2 = tree2.query(coordinates_test_img)[0]
+
         d1, d2 = mhd.calculate_minimum_distance_pairwise(self.edge_testing[0], self.edge_training[0])
-        self.assertEqual(d1.shape, (86,))
-        self.assertEqual(d2.shape, (75,))
+        self.assertTrue((d1 == distance_1).all())
+        self.assertTrue((d2 == distance_2).all())
 
     def test_mhd22(self):
+        coordinates_test_img = image_processing.coordinates(self.edge_testing[0])
+        coordinates_training_img = image_processing.coordinates(self.edge_training[0])
+        tree1 = cKDTree(coordinates_test_img)
+        tree2 = cKDTree(coordinates_training_img)
+        distance_1 = tree1.query(coordinates_training_img)[0]
+        distance_2 = tree2.query(coordinates_test_img)[0]
+        distance_1 = distance_1.mean()
+        distance_2 = distance_2.mean()
+        max_length = max(distance_1, distance_2)
+
+
         d1 = mhd.mhd_d22(self.edge_testing[0], self.edge_training[0])
-        self.assertEqual(d1, 0.9252521966042869)
+        self.assertEqual(d1, max_length)
